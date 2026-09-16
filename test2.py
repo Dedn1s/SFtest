b97b8ed27935f53ae30cec46da3f927b2c458394
Dedn1s <vdgermay@gmail.com>
2026-09-17 01:28:22 +0300

    new file:   test.ipynb 	new file:   test.py 	new file:   test2.py

---
+++ test2.py
@@ -0,0 +1,22 @@
+"""Игра по угадыванию числа
+   компьютер сам загадывает и угадывает"""
+
+import numpy as np
+
+start = 1
+finish = 101
+number = np.random.randint(start, finish)
+
+count = 0
+
+while True:
+    predict_number = np.random.randint(start, finish)
+    count+=1
+    
+    if predict_number < number:
+        start = predict_number + 1
+    elif predict_number > number:
+        finish = predict_number
+    else:
+        print(f"Компьютер угадал число за {count} попыток. Это было число {number}. Тест корректности - {predict_number}")
+        break
